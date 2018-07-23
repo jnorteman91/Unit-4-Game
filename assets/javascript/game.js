59793ee3ae172077c5dcf4b2196af9564d87f777
Jd0h Norteman <jnorteman91@gmail.com>
2018-07-22 21:17:46 -0400

    Unit-4-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var Random = Math.floor(Math.random() * 120 + 19)
     $("#number").text(Random);
 
-var genNumbers = []
+var genNumbers = [];
 var combined = 0;
 var win = 0;
 var loss = 0;
@@ -13,11 +13,33 @@ $("#lossCount").text(loss);
 function RandomNumbers(){
     for (var i = 0; i < 4; i++) {
         var num = Math.floor(Math.random() * 11 +1);
-    genNumbers.push(num);
+        genNumbers.push(num);
     }
 }
 
 RandomNumbers();
 
+function reset(){
+    Random = Math.floor(Math.random() * 120 + 19)
+    $("#number").text(Random);
+    genNumbers = [];
+    RandomNumbers();
+    combined = 0;
+    $("#total").text(combined);
+}
+
+function winner(){
+    alert("You've Won!");
+        win++;
+        $("#winCount").text(win);
+        reset();
+}
+
+function loser(){
+    alert("You've Lost!");
+    loss++;
+    $("lossCount").text(loss);
+    reset();
+}
 
 })
